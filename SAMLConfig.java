fdffffcc95802607b33066af6054ecf0aba0ebb3
CHAMSEDDINE <toujani.chams@gmail.com>
2025-03-28 01:18:52 +0100

    Update SAMLConfig.java

--- SAMLConfig.java
+++ SAMLConfig.java
@@ -1,73 +1,44 @@
-package com.example.saml.config;
+package com.example.config;
 
-import org.opensaml.saml2.metadata.provider.FilesystemMetadataProvider;
-import org.opensaml.saml2.metadata.provider.MetadataProvider;
-import org.opensaml.xml.parse.BasicParserPool;
+import org.opensaml.saml2.metadata.provider.MetadataProviderException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.saml.SAMLAuthenticationProvider;
 import org.springframework.security.saml.SAMLBootstrap;
+import org.springframework.security.saml.SAMLDiscovery;
 import org.springframework.security.saml.metadata.CachingMetadataManager;
-import org.springframework.security.saml.metadata.MetadataGenerator;
-import org.springframework.security.saml.metadata.MetadataGeneratorFilter;
-import org.springframework.security.saml.processor.SAMLProcessorImpl;
-import org.springframework.security.saml.websso.WebSSOProfileConsumerImpl;
+import org.springframework.security.saml.processor.*;
+import org.springframework.security.saml.websso.*;
 
-import java.io.File;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
 public class SAMLConfig {
 
     @Bean
-    public SAMLAuthenticationProvider samlAuthenticationProvider() {
-        SAMLAuthenticationProvider provider = new SAMLAuthenticationProvider();
-        provider.setForcePrincipalAsString(false);
-        return provider;
+    public SAMLProcessor samlProcessor() {
+        List<SAMLBinding> bindings = new ArrayList<>();
+        bindings.add(new HTTPRedirectDeflateBinding());
+        bindings.add(new HTTPPostBinding());
+        bindings.add(new HTTPArtifactBinding(new ArtifactResolutionProfileImpl(new HttpClientFactory().createDefaultHttpClient())));
+        bindings.add(new HTTPSOAP11Binding());
+        bindings.add(new HTTPPAOS11Binding());
+
+        return new SAMLProcessorImpl(bindings);
     }
 
     @Bean
-    public MetadataProvider metadataProvider() throws Exception {
-        File idpMetadataFile = new File("src/main/resources/idp-metadata.xml");
-        FilesystemMetadataProvider provider = new FilesystemMetadataProvider(idpMetadataFile);
-        provider.setParserPool(parserPool());
-        return provider;
+    public SAMLBootstrap samlBootstrap() {
+        return new SAMLBootstrap();
     }
 
     @Bean
-    public CachingMetadataManager metadataManager() throws Exception {
-        List<MetadataProvider> providers = Collections.singletonList(metadataProvider());
-        return new CachingMetadataManager(providers);
+    public SAMLDiscovery samlDiscovery() {
+        return new SAMLDiscovery();
     }
 
     @Bean
-    public MetadataGenerator metadataGenerator() {
-        MetadataGenerator generator = new MetadataGenerator();
-        generator.setEntityId("saml-sp");
-        generator.setIncludeDiscoveryExtension(false);
-        return generator;
-    }
-
-    @Bean
-    public MetadataGeneratorFilter metadataGeneratorFilter() {
-        return new MetadataGeneratorFilter(metadataGenerator());
-    }
-
-    @Bean
-    public SAMLProcessorImpl samlProcessor() {
-        return new SAMLProcessorImpl(null);
-    }
-
-    @Bean
-    public BasicParserPool parserPool() {
-        BasicParserPool pool = new BasicParserPool();
-        pool.setMaxPoolSize(10);
-        return pool;
-    }
-
-    @Bean
-    public WebSSOProfileConsumerImpl webSSOProfileConsumer() {
-        return new WebSSOProfileConsumerImpl();
+    public CachingMetadataManager metadataManager() throws MetadataProviderException {
+        return new CachingMetadataManager(new ArrayList<>());
     }
 }
